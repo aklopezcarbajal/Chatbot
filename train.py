a5b0af6c0a894567da0b44c8eb6cf6d37f2f2f5d
Ana Karen Lopez Carbajal <67447283+aklopezcarbajal@users.noreply.github.com>
2021-06-19 19:23:41 -0400

    Working on training.

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ stemmer = PorterStemmer()
 def bag_of_words(text, words):
 	text_words = [stemmer.stem(w.lower()) for w in text]
 
-	bag = np.zeros(len(words))
+	bag = np.zeros(len(words), dtype=np.float32)
 	for i, w in enumerate(words):
 		if w in text:
 			bag[i] = 1
@@ -54,18 +54,40 @@ X = np.array(X)
 y = np.array(y)
 
 dataset = ChatbotDataset(X,y)
-train_dataloader = DataLoader(dataset,batch_size=10,shuffle=True)
+train_dataloader = DataLoader(dataset,batch_size=8,shuffle=True)
 
 # Training model
-device = torch.device('cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else'cpu')
 
 input_size = len(words)
 layer_size = 5
 output_size = len(classes)
 
 net = Net(input_size, layer_size, output_size)
+net.to(device)
 
 # Loss function and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters(),lr=0.001)
 
+epochs = 500
+
+for epoch in range(epochs):
+	for (pattern, tag) in train_dataloader:
+		pattern = pattern.to(device)
+		tag = tag.to(dtype=torch.long).to(device)
+
+		optimizer.zero_grad()
+
+		#forward - backward - optimize
+		output = net(pattern)
+		loss = criterion(output, tag)
+		loss.backward()
+		optimizer.step()
+	if (epoch+1) % 10 == 0:
+		print('[epoch',epoch+1,'] loss: %.3f' %loss.item())
+
+print('Finished Training')
+
+# Save trained model
+model_dict = {}
