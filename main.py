da5581bc4977585177c9e01285afb544e6e93100
Ana Karen Lopez Carbajal <67447283+aklopezcarbajal@users.noreply.github.com>
2021-06-22 12:38:15 -0400

    Working on chat loop.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import torch
+import nltk
+#nltk.download('punkt')
 from model import Net
+from dataset import bag_of_words
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else'cpu')
 
@@ -13,3 +16,12 @@ net = Net(input_size, layer_size, output_size)
 net.to(device)
 net.load_state_dict(model_state)
 net.eval()
+
+words, labels = model_dict['data']
+
+while True:
+	string = input('You:')
+	if string.lower() == 'quit':
+		break
+
+	bag = bag_of_words(nltk.word_tokenize(string),words)
