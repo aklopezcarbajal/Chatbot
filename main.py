78a9e664d00c7e373dbdb56f2a761112ae80d511
Ana Karen Lopez Carbajal <67447283+aklopezcarbajal@users.noreply.github.com>
2021-08-02 19:58:47 -0400

    Update main.py
    Changed device to cpu.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import torch
 import nltk
 #nltk.download('punkt')
-from model import Net
-from utils import bag_of_words
 import json
 import random
+from model import Net
+from utils import bag_of_words
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else'cpu')
+device = torch.device('cpu')
 print('[INF] Device:', device)
 
 # Load intents file
@@ -20,7 +20,6 @@ input_size, layer_size, output_size = model_dict['dimensions']
 model_state = model_dict['model_state']
 
 net = Net(input_size, layer_size, output_size)
-net.to(device)
 net.load_state_dict(model_state)
 net.eval()
 
